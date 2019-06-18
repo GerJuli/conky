3673b7c4597420450f741b8bec02ebd8cc8aafda
Julian-Samuel Gebühr <julian-samuel@gebuehr.net>
2019-06-18 09:00:16 +0200

    Weather script only checks ever 60 seconds

--- weather.lua
+++ weather.lua
@@ -4,7 +4,7 @@ lasttime = os.time()
 function conky_weather()
 	currenttime = os.time()
 	timedifference = os.difftime(currenttime, lasttime)
-	if timedifference > 30 then
+	if timedifference > 60 then
 		local file = io.popen("curl -s \"http://wttr.in/Stuttgart?0?T\"")
 		output = file:read("*a")
 		file:close()
