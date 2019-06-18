15d805e18f0648358e4ac442b7810f7427dd3ba5
Julian-Samuel Gebühr <julian-samuel@gebuehr.net>
2019-06-18 08:59:44 +0200

    Weather script is fitted in

--- weather.lua
+++ weather.lua
@@ -1,5 +1,14 @@
 -- Gibt das Wetter aus
+output = "Keine Wetterdaten"
+lasttime = os.time()
 function conky_weather()
-	io.popen("curl -s \"https://wttr.in/Berlin?0T\"")
-	output = file:read("*a")
+	currenttime = os.time()
+	timedifference = os.difftime(currenttime, lasttime)
+	if timedifference > 30 then
+		local file = io.popen("curl -s \"http://wttr.in/Stuttgart?0?T\"")
+		output = file:read("*a")
+		file:close()
+		laststime = currenttime
+	end
+	return output
 end
