626a2dcf6b8f186ed42bc11457def6c7c6050f6e
Julian-Samuel Gebühr <julian-samuel@gebuehr.net>
2020-02-13 14:05:05 +0100

    Increase scrape time

--- weather.lua
+++ weather.lua
@@ -1,14 +1,16 @@
 -- Gibt das Wetter aus
 output = "Keine Wetterdaten"
 lasttime = os.time()
+first_time = 1
 function conky_weather()
 	currenttime = os.time()
 	timedifference = os.difftime(currenttime, lasttime)
-	if timedifference > 60 then
+	if timedifference > 3600 or first_time then
 		local file = io.popen("curl -s \"http://wttr.in/Stuttgart?0?T\"")
 		output = file:read("*a")
 		file:close()
 		laststime = currenttime
+		first_time = 0
 	end
 	return output
 end
